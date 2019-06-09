ac0d389a8692b22c2a71f8b8563170f553f5b989
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-09 10:44:05 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+
+end
+
+def using_unshift(array,string)
+
+end
+
+def using_pop(array)
+
+end
+
+def pop_with_args(array)
+
+end
+
+def using_shift(array)
+
+end
+
+def shift_with_args(array)
+
+end
+
+def using_concat(arr1, arr2)
+
+end
+
+def using_insert(arra, element)
+
+end
+
+def using_uniq(array)
+
+end
+
+def using_flatten(array)
+
+end
+
+def using_delete(arr,string)
+
+end
+
+def using_delete_at(array, integer)
+
+end
